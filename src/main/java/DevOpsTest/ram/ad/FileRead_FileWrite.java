0be632709200733366e9bae98174f0658df00901
vkulshrestha <kul_vivek@hotmail.com>
2017-08-28 21:45:01 +0530

    foreach loop1

--- src/main/java/DevOpsTest/ram/ad/FileRead_FileWrite.java
+++ src/main/java/DevOpsTest/ram/ad/FileRead_FileWrite.java
@@ -61,7 +61,7 @@ public class FileRead_FileWrite {
 			 System.out.println("fr.read returns :" +fr.read(arr));
 			 fr.read(arr);
 
-			 // for each type loop
+			 // for each type loop1
 			 for(char c:arr){
 				 System.out.print(c);
 			 }
